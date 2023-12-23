88294f4ebfebf826a84693ee62f35bfd04a4bfbb
Vaibhav Chaudhary <vchaudhary3@worldbankgroup.org>
2023-12-24 00:14:18 +0530

    use of hbs view engine

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 const publicFolderPath = path.join(__dirname, '../public');
 
+app.set("view engine", "hbs");
+
 app.use(express.static(publicFolderPath));
+
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render("index", {
+        username:"vaibhav"
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.write('this is about page..');
+    res.render("about", {
+        username:"vaibhav"
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('index page from route');
+});
+
+app.get('/about', (req, res) => {
+    res.write('about page from route');
     res.send();
 });
 
 app.listen(7000, () => {
     console.log('loaded');
-})
+});
